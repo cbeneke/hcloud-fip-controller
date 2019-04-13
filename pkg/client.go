f0f2ec087498d564518defcd03093e216240544f
Christian Beneke <c.beneke@wirelab.org>
2019-04-13 14:06:50 +0200

    Add run function and basic FIP parsing

--- pkg/client.go
+++ pkg/client.go
@@ -1,39 +1,68 @@
 package fipcontroller
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/hetznercloud/hcloud-go/hcloud"
+	"net"
 	"os"
 )
 
-
 type Configuration struct {
-	Token string
+	Token      string
 	FloatingIP string
 }
 
 type Client struct {
-	Config Configuration
-	HetznerClient *hcloud.Client
+	Client        *hcloud.Client
+	Configuration Configuration
 }
 
 func newClient() (*Client, error) {
-	client := Client{}
-
-	client.Config = Configuration{}
+	config := Configuration{}
 	file, err := os.Open("config.json")
 	if err != nil {
-		return nil, fmt.Errorf("Could not open config file: %v", err)
+		return nil, fmt.Errorf("could not open config file: %v", err)
 	}
 	defer file.Close()
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&client.Config)
+	err = decoder.Decode(&config)
 	if err != nil {
-		return nil, fmt.Errorf("Could not decode config: %v", err)
+		return nil, fmt.Errorf("could not decode config: %v", err)
 	}
 
-	client.HetznerClient = hcloud.NewClient(hcloud.WithToken(client.Config.Token))
+	hClient := hcloud.NewClient(hcloud.WithToken(config.Token))
 
-	return &client, nil
+	return &Client{hClient, config}, nil
+}
+
+func getFipID(ctx context.Context, fip string, client *Client) (id int, err error) {
+	ips, err := client.Client.FloatingIP.All(ctx)
+	if err != nil {
+		return -1, fmt.Errorf("could not fetch floating IPs: %v", err)
+	}
+
+	for _, ip := range ips {
+		if ip.IP.Equal(net.ParseIP(fip)) {
+			return ip.ID, nil
+		}
+	}
+
+	return -1, fmt.Errorf("IP address %s not allocated", fip)
+}
+
+func run() error {
+	ctx := context.Background()
+	client, err := newClient()
+	if err != nil {
+		return fmt.Errorf("could not initialise hetzner client: %v", err)
+	}
+	fip, err := getFipID(ctx, client.Configuration.FloatingIP, client)
+	if err != nil {
+		return fmt.Errorf("could not get floating IP: %v", err)
+	}
+
+	fmt.Printf("Using IP address %d.\n", fip)
+	return nil
 }
