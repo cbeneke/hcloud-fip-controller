d8b072acc501abda1ca512c144ede3bbcd45ea93
Matthias Loibl <mail@matthiasloibl.com>
2019-04-13 21:50:08 +0200

    Format imports with goimports
    I didn't do this change myself.
    Instead I always run goimports automatically:
    http://goinbigdata.com/goimports-vs-gofmt/

--- pkg/fipcontroller/client.go
+++ pkg/fipcontroller/client.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"time"
+
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"net"
-	"os"
-	"time"
 )
 
 const Version = "0.0.1"
