51657a7ac782ce30ec1b34e44d3447b05402397d
Phillip <phillip@freitagsrunde.org>
2020-05-11 22:49:54 +0200

    Add option to find nodes by deployment pods
    This commit adds an option to find the hetzner server through the
    deployed fip-controller pods.
    
    This is done by searching for a specific pod label that can be created
    with the deployment (label can be configured).
    
    If no pods are found this way it will try to get the servers the old way
    by finding the nodes

--- internal/app/fipcontroller/kubernetes.go
+++ internal/app/fipcontroller/kubernetes.go
@@ -32,8 +32,27 @@ func newKubernetesClient() (*kubernetes.Clientset, error) {
  *  Will return first found internal or external IP depending on nodeAddressType parameter
  */
 func (controller *Controller) nodeAddressList(ctx context.Context, nodeAddressType configuration.NodeAddressType) (addressList []net.IP, err error) {
-	// Create list options with optional labelSelector
+	// Try to get deployment pods if certain label is specified
 	listOptions := metav1.ListOptions{}
+	listOptions.LabelSelector = controller.Configuration.PodLabelSelector
+
+	pods, err := controller.KubernetesClient.CoreV1().Pods(controller.Configuration.Namespace).List(ctx, listOptions)
+	if err != nil {
+		return nil, fmt.Errorf("could not list nodes: %v", err)
+	}
+	controller.Logger.Debugf("Found %d pods", len(pods.Items))
+
+	for _, pod := range pods.Items {
+		address := net.ParseIP(pod.Status.HostIP)
+		addressList = append(addressList, address)
+	}
+	if len(addressList) > 0 {
+		controller.Logger.Debugf("Found %d ips from pods", len(addressList))
+		return
+	}
+
+	// Create list options with optional labelSelector
+	listOptions = metav1.ListOptions{}
 	if controller.Configuration.NodeLabelSelector != "" {
 		listOptions.LabelSelector = controller.Configuration.NodeLabelSelector
 	}
